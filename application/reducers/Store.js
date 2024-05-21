68f534b04360e6cc9c7ac440b311ef538402493d
joaquin-gopick <joakinchaparro@hotmail.com>
2024-05-21 17:42:50 +0200

    correción dependencias

--- application/reducers/Store.js
+++ application/reducers/Store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import rootReducer from "./RootReducer";
+const thunkMiddleware = require("redux-thunk").thunk;
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export default store;
